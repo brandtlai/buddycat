ee9293b91f6e52b8c029070c9d71584c79792814
laijiayu.ljy <laijiayu.ljy@antgroup.com>
2026-04-07 13:35:46 +0800

    Initial Commit

---
+++ BongoCat/BongoCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BongoCatApp.swift
+//  BongoCat
+//
+//  Created by sumori on 2026/4/7.
+//
+
+import SwiftUI
+
+@main
+struct BongoCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
